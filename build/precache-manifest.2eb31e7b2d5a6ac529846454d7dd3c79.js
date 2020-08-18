aa39f626ffe8ba50894601b792950fdfed5e8603
Donghyun Cho <cdhnet98@gmail.com>
2020-08-19 00:39:43 +0900

    Build demo website

---
+++ build/precache-manifest.2eb31e7b2d5a6ac529846454d7dd3c79.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80d676c87b2811acf13320b34e5d8277",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8b812b32729a88fbd7a",
+    "url": "/static/css/main.e6d2bf55.chunk.css"
+  },
+  {
+    "revision": "958b34065534b924ddbd",
+    "url": "/static/js/2.c4802bba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c4802bba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b812b32729a88fbd7a",
+    "url": "/static/js/main.f7fad012.chunk.js"
+  },
+  {
+    "revision": "43ee2160ac4b05ac422b",
+    "url": "/static/js/runtime-main.70e1a87e.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import krNumbers from "kr-numbers";
+import "./App.css";
+
+function App() {
+  const [text, setText] = useState("");
+  const [spacing, setSpacing] = useState(false);
+  const [mixed, setMixed] = useState(false);
+
+  const handleChange = (e) => {
+    const { value } = e.target;
+
+    const isInteger = Number.isInteger(Number(value));
+    const isExceededMaxLength = value.length > 21;
+
+    if (isInteger && !isExceededMaxLength) {
+      setText(value);
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1> kr-numbers.js </h1>
+      <h2> $ npm install kr-numbers --save </h2>
+      <input
+        type="text"
+        placeholder="Input numbers here"
+        onChange={handleChange}
+        value={text}
+      />
+      <div className="checkbox-wrapper">
+        <input
+          id="spacing"
+          type="checkbox"
+          onChange={() => setSpacing(!spacing)}
+        />
+        <label htmlFor="spacing">Spacing</label>
+
+        <input id="mixed" type="checkbox" onChange={() => setMixed(!mixed)} />
+        <label htmlFor="mixed">Mixed</label>
+      </div>
+      <div className="result">
+        {krNumbers(Number(text), { spacing, mixed }) ||
+          "Please input your numbers"}
+      </div>
+    </div>
+  );
+}
+
+export default App;
